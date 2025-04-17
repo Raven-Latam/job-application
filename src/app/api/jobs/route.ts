2c90c53be9639b0e02936a59b047d74329eed341
jesus <jesus.rondon@raven.inc>
2025-04-17 13:47:01 -0400

    db.json cambió de ubicación a ver si funciona en vercel

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import jobsData from '@/../db.json';
+import jobsData from '@/app/data/db.json';
 
 export async function GET() {
   return NextResponse.json(jobsData.jobs);
