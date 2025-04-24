78b94bb9a6fde8ebf8a9a645b29285110a46bf65
jesus <jesus.rondon@raven.inc>
2025-04-24 10:58:40 -0400

    cambié el page.tsx a que llame al componente JobList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import SEO_DATA from '@/constants/SEO_DATA/SEO_DATA';
 import getMetadata from '@/utils/seo/get-metadata';
-//import JobList from '@/components/JobList';
+import JobList from '@/components/JobList';
 import '../styles/globals.css';
 
 export const metadata = getMetadata(SEO_DATA.index);
 
-/*export default function Page() {
-  return <JobList />;
-}*/
-
-// src/app/page.tsx
 export default function Page() {
-  return <h1>¡Hola Mundo!</h1>;
+  return <JobList />;
 }
