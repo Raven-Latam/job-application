3651089cf1ce35efb11cc284da362571e38c4e97
jesus <jesus.rondon@raven.inc>
2025-04-24 10:30:01 -0400

    correcion en page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import SEO_DATA from '@/constants/SEO_DATA/SEO_DATA';
 import getMetadata from '@/utils/seo/get-metadata';
-import JobList from '@/components/JobList';
+//import JobList from '@/components/JobList';
 import '../styles/globals.css';
 
 export const metadata = getMetadata(SEO_DATA.index);
